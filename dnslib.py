89065aab04d4e694fb76b30f7b1358fa7b17f862
Kristov Atlas <kristovatlas@gmail.com>
2017-01-06 23:08:01 -0500

    fix whitespace to make pylint happy
    TODO: more pylinting of this file

--- dnslib.py
+++ dnslib.py
@@ -8,7 +8,7 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
@@ -22,100 +22,101 @@
 
 from dnstwist import DomainFuzz
 try:
-	import dns.resolver
-	module_dnspython = True
+    import dns.resolver
+    module_dnspython = True
 except:
-	module_dnspython = False
-	pass
+    module_dnspython = False
+    pass
 try:
-	import whois
-	module_whois = True
+    import whois
+    module_whois = True
 except:
-	module_whois = False
-	pass
+    module_whois = False
+    pass
 
-class dnslib:		
-	
-	def __init__(self):
-		#Putting this here for now as a placeholder for the dynamic fuzzing code I'm working on.
-		self.domains = []
+class dnslib:
 
-	def checkDomain(self,dnsEntryName):
-		fuzzer = DomainFuzz(dnsEntryName.lower())
-		fuzzer.generate()
-		domains = fuzzer.domains
-	
-		total_hits = 0
-	
-		for i in range(0, len(domains)):
-			if module_dnspython:
-				resolv = dns.resolver.Resolver()
-				resolv.lifetime = 1
-				resolv.timeout = 1
-	
-				try:
-					ns = resolv.query(domains[i]['domain-name'], 'NS')
-					domains[i]['ns'] = str(ns[0])[:-1].lower()
-				except:
-					pass
-	
-				if 'ns' in domains[i]:
-					try:
-						ns = resolv.query(domains[i]['domain-name'], 'A')
-						domains[i]['a'] = str(ns[0])
-					except:
-						pass
-		
-					try:
-						ns = resolv.query(domains[i]['domain-name'], 'AAAA')
-						domains[i]['aaaa'] = str(ns[0])
-					except:
-						pass
-	
-					try:
-						mx = resolv.query(domains[i]['domain-name'], 'MX')
-						domains[i]['mx'] = str(mx[0].exchange)[:-1].lower()
-					except:
-						pass
-	
-			if 'ns' in domains[i] or 'a' in domains[i]:
-				try:
-					whoisdb = whois.query(domains[i]['domain-name'])
-					domains[i]['created'] = str(whoisdb.creation_date).replace(' ', 'T')
-					domains[i]['updated'] = str(whoisdb.last_updated).replace(' ', 'T')
-				except:
-					pass
-		
-		returnDomains = []
-		for i in domains:
-			info = ''
-	
-			if 'a' in i:
-				info += i['a']
-				if 'country' in i:
-					info += '/' + i['country']
-				if 'banner-http' in i:
-					info += ' HTTP:"%s"' % i['banner-http']
-			elif 'ns' in i:
-				info += 'NS:' + i['ns']
-	
-			if 'aaaa' in i:
-				info += ' ' + i['aaaa']
-	
-			if 'mx' in i:
-				info += ' MX:' + i['mx']
-				if 'banner-smtp' in i:
-					info += ' SMTP:"%s"' % i['banner-smtp']
-	
-			if 'created' in i and 'updated' in i and i['created'] == i['updated']:
-				info += ' Created/Updated:' + i['created']
-			else:
-				if 'created' in i:
-					info += ' Created:' + i['created']
-				if 'updated' in i:
-					info += ' Updated:' + i['updated']
-	
-			if info:
-				returnDomains.append([i['domain-name'],info])	
-	
-		return returnDomains
+    def __init__(self):
+        #Putting this here for now as a placeholder for the dynamic fuzzing code I'm working on.
+        self.domains = []
+
+    def checkDomain(self,dnsEntryName):
+        """TODO: describe arguments and return value!"""
+        fuzzer = DomainFuzz(dnsEntryName.lower())
+        fuzzer.generate()
+        domains = fuzzer.domains
+
+        total_hits = 0
+
+        for i in range(0, len(domains)):
+            if module_dnspython:
+                resolv = dns.resolver.Resolver()
+                resolv.lifetime = 1
+                resolv.timeout = 1
+
+                try:
+                    ns = resolv.query(domains[i]['domain-name'], 'NS')
+                    domains[i]['ns'] = str(ns[0])[:-1].lower()
+                except:
+                    pass
+
+                if 'ns' in domains[i]:
+                    try:
+                        ns = resolv.query(domains[i]['domain-name'], 'A')
+                        domains[i]['a'] = str(ns[0])
+                    except:
+                        pass
+
+                    try:
+                        ns = resolv.query(domains[i]['domain-name'], 'AAAA')
+                        domains[i]['aaaa'] = str(ns[0])
+                    except:
+                        pass
+
+                    try:
+                        mx = resolv.query(domains[i]['domain-name'], 'MX')
+                        domains[i]['mx'] = str(mx[0].exchange)[:-1].lower()
+                    except:
+                        pass
+
+            if 'ns' in domains[i] or 'a' in domains[i]:
+                try:
+                    whoisdb = whois.query(domains[i]['domain-name'])
+                    domains[i]['created'] = str(whoisdb.creation_date).replace(' ', 'T')
+                    domains[i]['updated'] = str(whoisdb.last_updated).replace(' ', 'T')
+                except:
+                    pass
+
+        returnDomains = []
+        for i in domains:
+            info = ''
+
+            if 'a' in i:
+                info += i['a']
+                if 'country' in i:
+                    info += '/' + i['country']
+                if 'banner-http' in i:
+                    info += ' HTTP:"%s"' % i['banner-http']
+            elif 'ns' in i:
+                info += 'NS:' + i['ns']
+
+            if 'aaaa' in i:
+                info += ' ' + i['aaaa']
+
+            if 'mx' in i:
+                info += ' MX:' + i['mx']
+                if 'banner-smtp' in i:
+                    info += ' SMTP:"%s"' % i['banner-smtp']
+
+            if 'created' in i and 'updated' in i and i['created'] == i['updated']:
+                info += ' Created/Updated:' + i['created']
+            else:
+                if 'created' in i:
+                    info += ' Created:' + i['created']
+                if 'updated' in i:
+                    info += ' Updated:' + i['updated']
+
+            if info:
+                returnDomains.append([i['domain-name'],info])
+
+        return returnDomains
